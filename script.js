e5d6bfc0159c71943d3ecdb170c0fd091a4586d7
Alejandro Daniel Polak <alejandro_polak@hotmail.com>
2019-11-01 14:00:08 -0300

    subido 1-1-19

---
+++ script.js
@@ -0,0 +1,15 @@
+({
+  babel: true
+})
+
+var abc = 123;
+
+abc
+
+
+  /* =======Funcion cambio CAMBIO NOCHE DIA =======*/
+function swapDayAndNight()
+        {
+            let body= document.getElementById('body');
+            body.classList.toggle('dark-theme');
+        }
